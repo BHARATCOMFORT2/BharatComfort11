fd338195279decd78503d70d17f947f0ca407eea
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-11 20:17:27 +0530

    Create dictionaries.ts

---
+++ app/i18n/dictionaries.ts
@@ -0,0 +1,11 @@
+// Loads translation files dynamically
+import "server-only";
+
+const dictionaries = {
+  en: () => import("./locales/en.json").then((m) => m.default),
+  hi: () => import("./locales/hi.json").then((m) => m.default),
+  fr: () => import("./locales/fr.json").then((m) => m.default),
+};
+
+export const getDictionary = async (locale: string) =>
+  dictionaries[locale as keyof typeof dictionaries]?.() ?? dictionaries.en();

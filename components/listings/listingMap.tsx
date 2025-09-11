3ac5584d1dde2050b237ae65ef3cb3578a111f6f
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-11 20:48:59 +0530

    Create listingMap.tsx

---
+++ components/listings/listingMap.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const markerIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+type ListingMapProps = {
+  lat: number;
+  lng: number;
+  title: string;
+};
+
+export default function ListingMap({ lat, lng, title }: ListingMapProps) {
+  return (
+    <div className="w-full h-[400px] rounded-lg overflow-hidden shadow">
+      <MapContainer
+        center={[lat, lng]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]} icon={markerIcon}>
+          <Popup>{title}</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}

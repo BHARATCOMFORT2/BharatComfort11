005619c818b132f1f8352e597f207ea9e8576e44
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-13 17:26:46 +0530

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindFunctions: ["clsx", "classnames"],
+};

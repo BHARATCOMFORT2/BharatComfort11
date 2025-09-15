573ea3cb6d08be3769d51688d0fd6faa7d4f2cf0
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-15 16:38:50 +0530

    Update page.tsx

--- app/partner/listings/new/page.tsx
+++ app/partner/listings/new/page.tsx
@@ -21,8 +21,8 @@ export default function NewListingPage() {
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-  ) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+  ) => 
+    {setForm({ ...form, [e.target.name]: e.target.value });
+    };
 
   const handleSubmit = async (e: React.FormEvent) => {

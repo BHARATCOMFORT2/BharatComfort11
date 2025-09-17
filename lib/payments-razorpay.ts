ef284ca7f55d239622706fd0238bcd1ab880a3f8
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-17 15:41:12 +0530

    Update payments-razorpay.ts

--- lib/payments-razorpay.ts
+++ lib/payments-razorpay.ts
@@ -4,6 +4,10 @@ const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_KEY_SECRET!,
 });
+function createOrder(params: any) {
+}
+
+export { createOrder };
 
 export async function createRazorpayOrder(amount: number, currency: string = "INR") {
   const options = {

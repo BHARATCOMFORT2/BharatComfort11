8ee675187b081a819e1c4d5d8e13bb889593a6a9
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-20 07:11:10 +0530

    Update page.tsx

--- app/payments/manage/page.tsx
+++ app/payments/manage/page.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "@/hooks/useAuth";
 import { useEffect, useState } from "react";
 import { getFirestore } from "firebase/firestore";
-import { app } from "/firebase";
+import { db } from "@/lib/firebase"; // ✅ correct
 export const db = getFirestore(app);
 import { collection, query, where, getDocs } from "firebase/firestore";
 

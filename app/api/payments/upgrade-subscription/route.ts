8500c854be990df90b5fcc4e27cbd7ad0c2fc60f
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-24 12:36:15 +0530

    Update route.ts

--- app/api/payments/upgrade-subscription/route.ts
+++ app/api/payments/upgrade-subscription/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+  key_id: rzp_test_RLLXPFy8ccQnkb,
+  key_secret:VWujaCB0uEuuM4PfONs2gF8g,
 });
 
 export async function POST(req: Request) {

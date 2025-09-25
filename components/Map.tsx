cc55b67ca49165cfbe846e5f8dffc3ba36569357
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-25 05:45:12 +0530

    Create Map.tsx

---
+++ components/Map.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix Leaflet default icon paths
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[28.6139, 77.209]} // Example: Delhi, India
+      zoom={5}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+      />
+      <Marker position={[28.6139, 77.209]}>
+        <Popup>Delhi, India</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}

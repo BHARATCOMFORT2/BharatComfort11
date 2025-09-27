6cf2c2766d28e7f02796f96655e24e782bc97ed3
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-27 08:04:23 +0530

    Update route.ts

--- app/api/payments/razorpay/route.ts
+++ app/api/payments/razorpay/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import razorpay from "@/lib/payments-razorpay";
+import { razorpay } from "@/lib/payments-razorpay";
+
 
 export async function POST(req: Request) {
   try {

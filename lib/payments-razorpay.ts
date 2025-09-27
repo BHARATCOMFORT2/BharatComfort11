d8b1187e2de04e782fb9ad4e7064317a2e120372
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-27 07:57:17 +0530

    Update payments-razorpay.ts

--- lib/payments-razorpay.ts
+++ lib/payments-razorpay.ts
@@ -2,6 +2,11 @@
 import Razorpay from "razorpay";
 import crypto from "crypto";
 
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID!,
+  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+});
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_KEY_SECRET!,

a458b2473f281f277add0ff6c19690da31849835
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-09-28 07:34:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 import { Inter, Poppins } from "next/font/google";
 

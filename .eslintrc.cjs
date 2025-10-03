294f9380e965fb8d23fd809225849e477ac20076
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-10-03 12:52:11 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,15 +7,25 @@ module.exports = {
     sourceType: "module",
     project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint"], // ✅ removed react + react-hooks (already in next/core-web-vitals)
+  plugins: ["@typescript-eslint"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    // Relax rules so you can build faster
-    "@typescript-eslint/no-explicit-any": "off", // allow "any"
-    "no-console": "off", // allow console.log, console.error, etc.
-    "react-hooks/exhaustive-deps": "warn", // show warning instead of error
+    // TypeScript
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+
+    // React
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-unescaped-entities": "off",
+
+    // Next.js
+    "@next/next/no-img-element": "off",
+
+    // General
+    "no-console": "off",
   },
 };

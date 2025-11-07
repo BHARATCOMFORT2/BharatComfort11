c6d12c9154747e675738114a0940a06e71306e63
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-11-07 18:39:30 +0530

    Update page.tsx

--- app/user/dashboard/booking-details/[id]/page.tsx
+++ app/user/dashboard/booking-details/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { toast } from "sonner";
 import { getFirebaseIdToken } from "@/lib/firebase-auth";
 

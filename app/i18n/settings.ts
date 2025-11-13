d9296e653c4d4966a10cec35f67b8cacabad2bc8
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2025-11-13 17:19:01 +0530

    Delete app/i18n/settings.ts

--- app/i18n/settings.ts
+++
@@ -1,8 +0,0 @@
-// app/i18n/settings.ts
-
-export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "hi"], // add more locales like "fr", "de", etc.
-};
-
-export type Locale = (typeof i18n)["locales"][number];

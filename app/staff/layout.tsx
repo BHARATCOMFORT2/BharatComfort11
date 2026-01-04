cddc0b9bd187de62cfa963ecc19d88c232d383e5
BHARATCOMFORT2 <shrrajbhar12340@gmail.com>
2026-01-04 10:16:38 +0530

    Create layout.tsx

---
+++ app/staff/layout.tsx
@@ -0,0 +1,16 @@
+import StaffSidebar from "@/components/sidebar/StaffSidebar";
+
+export default function StaffLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen">
+      <StaffSidebar />
+      <main className="flex-1 bg-gray-50">
+        {children}
+      </main>
+    </div>
+  );
+}
